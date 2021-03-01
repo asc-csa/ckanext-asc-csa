0cf005e6c1478740903a7d40f6c557de130ba432
nfee006 <nfee006@uottawa.ca>
2021-03-01 12:48:05 -0800

    Update Harvester_example.py
    Added french comments, fixed print statements

--- Harvester_example.py
+++ Harvester_example.py
@@ -5,9 +5,11 @@ import pandas as pd
 import numpy as np
 import time
 
+
 def get_data():
+    # Demander une liste des ensembles de données qui ont le id csa-asc
     # Request a list of datasets that have the id csa-asc
-    response = requests.get('https://open.canada.ca/data/en/api/3/action/organization_show?id=csa-asc&include_datasets=True') #to change when public
+    response = requests.get('https://open.canada.ca/data/en/api/3/action/organization_show?id=csa-asc&include_datasets=True')
     response.encoding = "utf-8"
     gov_canada_api_datasets = json.loads(response.text)
 
@@ -20,8 +22,9 @@ def get_data():
 
     datasets = []
 
-    print "This will take a few minutes...."
+    print("Cela prendra quelques minutes.... | This will take a few minutes....")
 
+    # Demander les ensembles de données par leur identifiant pour obtenir leurs métadonnées complètes
     # Request datasets by their id to get their full metadata
     l=len(dataset_ids)
 
@@ -31,22 +34,22 @@ def get_data():
             i+=1
             try :
 
-                response = requests.get('https://open.canada.ca/data/en/api/3/action/package_show?id=' + dataset_id)
+                response = requests.get("https://open.canada.ca/data/en/api/3/action/package_show?id=" + dataset_id)
                 response.encoding = "utf-8"
                 gov_canada_api_dataset = json.loads(response.text)
                 assert (gov_canada_api_dataset['success'] == True)
                 datasets.append(gov_canada_api_dataset)
-                print i,'/',l
+                print(i,'/',l)
                 time.sleep(1)
 
             except :
-                print('An error occured and this dataset will be skipped!')
+                print("Une erreur s'est produite et cet ensemble de données sera ignoré ! | An error occurred and this dataset will be skipped!")
                 l-=1
                 time.sleep(1)
 
-    # Save data that was requested into .json
-    # Could be a problem if there is too much data to hold in memory if this script is used on another instance of CKAN
+    # Sauvegarder les données demandées dans un fichier .json
+    # Save the data that was requested into a .json file
     with open('gov_canada_datasets_raw.json', 'w') as f:
         json.dump(datasets, f)
 
-    print "Success!!"
+    print("Succès ! | Success!")
