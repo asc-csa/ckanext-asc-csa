d37515d1b0d69318492c7dbaa2d198e6bf1642c8
Tyler Kennedy <tk@tkte.ch>
2025-05-26 05:29:08 -0400

    Linting, py2-to-3 fixes.

--- ckanext/csa/plugin.py
+++ ckanext/csa/plugin.py
@@ -6,19 +6,15 @@ import os
 import inspect
 
 # from paste.reloader import watch_file
-from ckan.common import config
-from ckan.common import request
 from ckan.lib.plugins import DefaultTranslation
-from ckan.plugins.toolkit import _, url_for, redirect_to, request, config
+from ckan.plugins.toolkit import _, request
 from ckanext.csa import helpers
 from ckanext.csa import loader
 
 # from ckanext.csa import blueprint
-from flask import Blueprint, redirect
 
 import ckanext.csa.blueprint as blueprint
 
-import ckan.lib.base as base
 import routes.mapper
 # class _CsaMixin(object):
 #     """
@@ -35,7 +31,6 @@ import routes.mapper
 #         for f in reversed(field_descriptions):
 #             for pp in _loadschema(f)['presets']:
 #         _field_descriptions
-
 
 
 # def load_field_description_module_path(url):
@@ -55,6 +50,7 @@ import routes.mapper
 #         watch_file(p)
 #         # print loader.load(open(p))
 #         return loader.load(open(p))
+
 
 class CsaPlugin(p.SingletonPlugin, DefaultTranslation):
     # p.implements(p.IDatasetForm)
@@ -84,8 +80,8 @@ class CsaPlugin(p.SingletonPlugin, DefaultTranslation):
             return
         CsaPlugin._field_descriptions = {}
         file = "ckanext.csa:field_descriptions.json"
-        for p in self._load_files(file)['descriptions']:
-            CsaPlugin._field_descriptions[p['field_name']] = p['description']
+        for preset in self._load_files(file)["descriptions"]:
+            CsaPlugin._field_descriptions[preset["field_name"]] = preset["description"]
 
     def _load_files(self, url):
         """
@@ -93,10 +89,10 @@ class CsaPlugin(p.SingletonPlugin, DefaultTranslation):
         find the second part relative to the import path of the first
         *used for JS field_descriptions
         """
-        module, file_name = url.split(':', 1)
+        module, file_name = url.split(":", 1)
         try:
             # __import__ has an odd signature
-            m = __import__(module, fromlist=[''])
+            m = __import__(module, fromlist=[""])
         except ImportError:
             return
         p = os.path.join(os.path.dirname(inspect.getfile(m)), file_name)
@@ -104,84 +100,79 @@ class CsaPlugin(p.SingletonPlugin, DefaultTranslation):
             # watch_file(p)
             return loader.load(open(p))
 
-    #IpackageController
+    # IpackageController
     def before_search(self, search_params):
-        u'''Extensions will receive a dictionary with the query parameters,
+        """Extensions will receive a dictionary with the query parameters,
         and should return a modified (or not) version of it.
-        '''
-        query_fields = ''
+        """
+        query_fields = ""
         # get current language
         try:
-            current_lang = request.environ['CKAN_LANG']
+            current_lang = request.environ["CKAN_LANG"]
         except TypeError as err:
-            if err.message == ('No object (name: request) has been registered '
-                               'for this thread'):
-                current_lang = 'en'
+            if err.message == (
+                "No object (name: request) has been registered " "for this thread"
+            ):
+                current_lang = "en"
             else:
                 raise
         except KeyError:
-            current_lang = 'en'
+            current_lang = "en"
 
         # Dismax search term for French
-        if current_lang == 'fr':
-            query_fields = 'title_fr^8 text_french^4 title^2 text'
+        if current_lang == "fr":
+            query_fields = "title_fr^8 text_french^4 title^2 text"
         # code below to potentially search equal parameters English, although currently defaults to CKAN core search
         # else if current_lang == 'en':
         #     query_fields = 'title^8 text^4 title_fr^2 text_french'
 
         if query_fields:
-            search_params['qf'] = query_fields
-
+            search_params["qf"] = query_fields
 
         return search_params
 
     def after_search(self, search_results, search_params):
         return search_results
 
+    # Before index runs before SOLR does an index/reindex
+    # SOLR can be reindexed with the command 'search-index rebuild -r'
+    # Must also edit schema.xml for these changes to help
 
-
-    #Before index runs before SOLR does an index/reindex
-    #SOLR can be reindexed with the command 'search-index rebuild -r'
-    #Must also edit schema.xml for these changes to help
-
-    #Implements bilingual searching
+    # Implements bilingual searching
     def before_index(self, pkg_dict):
+        print("RAN before_index")
 
-        print('RAN before_index')
+        pkg_dict["subject"] = json.loads(pkg_dict.get("subject", "[]"))
+        pkg_dict["project"] = json.loads(pkg_dict.get("project", "[]"))
 
-        pkg_dict['subject'] = json.loads(pkg_dict.get('subject', '[]'))
-        pkg_dict['project'] = json.loads(pkg_dict.get('project', '[]'))
+        kw = json.loads(pkg_dict.get("extras_keywords", "{}"))
+        pkg_dict["keywords_en"] = kw.get("en", [])
+        pkg_dict["keywords_fr"] = kw.get("fr", [])
 
-        kw = json.loads(pkg_dict.get('extras_keywords', '{}'))
-        pkg_dict['keywords_en'] = kw.get('en', [])
-        pkg_dict['keywords_fr'] = kw.get('fr', [])
+        notes = json.loads(pkg_dict.get("extras_notes_translated", "{}"))
+        pkg_dict["notes_en"] = notes.get("en", "")
+        pkg_dict["notes_fr"] = notes.get("fr", "")
 
+        titles = json.loads(pkg_dict.get("title_translated", "{}"))
+        pkg_dict["title_fr"] = titles.get("fr", "")
+        pkg_dict["title_string"] = titles.get("en", "")
 
-
-        notes = json.loads(pkg_dict.get('extras_notes_translated', '{}'))
-        pkg_dict['notes_en'] = notes.get('en', u'')
-        pkg_dict['notes_fr'] = notes.get('fr', u'')
-
-        titles = json.loads(pkg_dict.get('title_translated', '{}'))
-        pkg_dict['title_fr'] = titles.get('fr', u'')
-        pkg_dict['title_string'] = titles.get('en', u'')
-
-        validated_data_dict = json.loads(pkg_dict.get('validated_data_dict'))
+        validated_data_dict = json.loads(pkg_dict.get("validated_data_dict"))
         res_name_fr = []
         if validated_data_dict:
-            resources = validated_data_dict.get('resources')
+            resources = validated_data_dict.get("resources")
             for resource in resources:
-                res_name_fr_temp = resource.get('name_translated', {}).get('fr')
+                res_name_fr_temp = resource.get("name_translated", {}).get("fr")
                 if res_name_fr_temp:
                     res_name_fr.append(res_name_fr_temp)
-        pkg_dict['res_name_fr'] = res_name_fr
+        pkg_dict["res_name_fr"] = res_name_fr
 
         self.pkg_dict = pkg_dict
 
         return pkg_dict
 
     def before_view(self, pkg_dict):
-        print('RAN before_view')
+        print("RAN before_view")
         return pkg_dict
 
     def read(self, entity):
@@ -216,108 +207,111 @@ class CsaPlugin(p.SingletonPlugin, DefaultTranslation):
     def update_config(self, config_):
         # Add template and resource directories to access custom html/css files
         # Further documentation available in the CKAN official documentation
-        tk.add_template_directory(config_, 'templates')
-        tk.add_public_directory(config_, 'public')
-        tk.add_resource('fanstatic', 'csa')
-        self._load_field_descriptions('test')
+        tk.add_template_directory(config_, "templates")
+        tk.add_public_directory(config_, "public")
+        tk.add_resource("fanstatic", "csa")
+        self._load_field_descriptions("test")
         self._store_instance(self)
 
     def get_helpers(self):
         # Helpers for templates
         # Taken from helpers.py file
         return {
-            'csa_normalize_strip_accents': helpers.normalize_strip_accents,
-            'get_license': helpers.get_license,
-            'csa_get_translated_t': helpers.get_translated_t,
-            'csa_get_field_descriptions': helpers.csa_get_field_descriptions,
-            'csa_get_field_description': helpers.csa_get_field_description,
-            'get_translated_t' : helpers.get_translated_t,
-            'header_embeds_exists' : helpers.header_embeds_exists,
-            'footer_embeds_exists' : helpers.footer_embeds_exists,
-            'get_pkg_dict' : self.get_pkg_dict
-            }
-
+            "csa_normalize_strip_accents": helpers.normalize_strip_accents,
+            "get_license": helpers.get_license,
+            "csa_get_translated_t": helpers.get_translated_t,
+            "csa_get_field_descriptions": helpers.csa_get_field_descriptions,
+            "csa_get_field_description": helpers.csa_get_field_description,
+            "get_translated_t": helpers.get_translated_t,
+            "header_embeds_exists": helpers.header_embeds_exists,
+            "footer_embeds_exists": helpers.footer_embeds_exists,
+            "get_pkg_dict": self.get_pkg_dict,
+        }
 
     # IFacets
     # Implements custom facetting
 
     def dataset_facets(self, facets_dict, package_type):
-        print('RAN FACETS THING DATASET')
+        print("RAN FACETS THING DATASET")
         # facets_dict['division'] = p.toolkit._('Division')
-        facets_dict.update({
-            'portal_type': _('Data or information'),
-            'collection': _('Collection type'),
-            'science_admin': _('Science or management'),
-            'keywords_en': _('Keywords'),
-            'keywords_fr': _('Keywords'),
-            'keywords': _('Keywords'),
-            'project': _('CSA Science category'),
-            'res_format': _('Format'),
-            'frequency': _('Maintenance and update frequency'),
-            'topic_category': _('Topic categories'),
-            'spatial_representation_type': _('Spatial representation type'),
-            'fgp_viewer': _('Map viewer'),
-            'ready_to_publish': _('Record status'),
-            'imso_approval': _('IMSO approval'),
-            'jurisdiction': _('Jurisdiction'),
-            })
-        facets_dict['vocab_project'] = tk._('Project')
+        facets_dict.update(
+            {
+                "portal_type": _("Data or information"),
+                "collection": _("Collection type"),
+                "science_admin": _("Science or management"),
+                "keywords_en": _("Keywords"),
+                "keywords_fr": _("Keywords"),
+                "keywords": _("Keywords"),
+                "project": _("CSA Science category"),
+                "res_format": _("Format"),
+                "frequency": _("Maintenance and update frequency"),
+                "topic_category": _("Topic categories"),
+                "spatial_representation_type": _("Spatial representation type"),
+                "fgp_viewer": _("Map viewer"),
+                "ready_to_publish": _("Record status"),
+                "imso_approval": _("IMSO approval"),
+                "jurisdiction": _("Jurisdiction"),
+            }
+        )
+        facets_dict["vocab_project"] = tk._("Project")
         return facets_dict
 
     def group_facets(self, facets_dict, group_type, package_type):
-        print('RAN FACETS THING GROUP')
-        facets_dict.update({
-            'portal_type': _('Portal type'),
-            'collection': _('Collection type'),
-            'science_admin': _('Data category'),
-            'keywords_en': _('Keywords'),
-            'keywords_fr': _('Keywords'),
-            'keywords': _('Keywords'),
-            'subject': _('Subject'),
-            'res_format': _('Format'),
-            'res_type': _('Resource type'),
-            'frequency': _('Maintenance and update frequency'),
-            'topic_category': _('Topic Categories'),
-            'spatial_representation_type': _('Spatial representation type'),
-            'fgp_viewer': _('Map viewer'),
-            'ready_to_publish': _('Record status'),
-            'imso_approval': _('IMSO approval'),
-            'jurisdiction': _('Jurisdiction'),
-            })
+        print("RAN FACETS THING GROUP")
+        facets_dict.update(
+            {
+                "portal_type": _("Portal type"),
+                "collection": _("Collection type"),
+                "science_admin": _("Data category"),
+                "keywords_en": _("Keywords"),
+                "keywords_fr": _("Keywords"),
+                "keywords": _("Keywords"),
+                "subject": _("Subject"),
+                "res_format": _("Format"),
+                "res_type": _("Resource type"),
+                "frequency": _("Maintenance and update frequency"),
+                "topic_category": _("Topic Categories"),
+                "spatial_representation_type": _("Spatial representation type"),
+                "fgp_viewer": _("Map viewer"),
+                "ready_to_publish": _("Record status"),
+                "imso_approval": _("IMSO approval"),
+                "jurisdiction": _("Jurisdiction"),
+            }
+        )
         return facets_dict
 
     def organization_facets(self, facets_dict, organization_type, package_type):
-        print('RAN FACETS THING ORGANIZATION')
-        facets_dict.update({
-            'portal_type': _('Portal type'),
-            'collection': _('Collection type'),
-            'science_admin': _('Data category'),
-            'keywords_en': _('Keywords'),
-            'keywords_fr': _('Keywords'),
-            'keywords': _('Keywords'),
-            'subject': _('Subject'),
-            'res_format': _('Format'),
-            'res_type': _('Resource type'),
-            'frequency': _('Maintenance and update frequency'),
-            'topic_category': _('Topic categories'),
-            'spatial_representation_type': _('Spatial representation type'),
-            'fgp_viewer': _('Map viewer'),
-            'ready_to_publish': _('Record status'),
-            'imso_approval': _('IMSO approval'),
-            'jurisdiction': _('Jurisdiction'),
-            })
+        print("RAN FACETS THING ORGANIZATION")
+        facets_dict.update(
+            {
+                "portal_type": _("Portal type"),
+                "collection": _("Collection type"),
+                "science_admin": _("Data category"),
+                "keywords_en": _("Keywords"),
+                "keywords_fr": _("Keywords"),
+                "keywords": _("Keywords"),
+                "subject": _("Subject"),
+                "res_format": _("Format"),
+                "res_type": _("Resource type"),
+                "frequency": _("Maintenance and update frequency"),
+                "topic_category": _("Topic categories"),
+                "spatial_representation_type": _("Spatial representation type"),
+                "fgp_viewer": _("Map viewer"),
+                "ready_to_publish": _("Record status"),
+                "imso_approval": _("IMSO approval"),
+                "jurisdiction": _("Jurisdiction"),
+            }
+        )
         return facets_dict
 
-    #IRoutesl
+    # IRoutesl
     def before_map(self, route_map):
-        print('BEFORE MAP TIHASFDJSBDFKJSDFKJHSDF')
-        route_map.redirect('/', '/dataset')
-        with routes.mapper.SubMapper(route_map,controller='ckanext.csa.plugin:CSAController') as m:
-                    m.connect(
-                        'API',
-                        '/API',
-                        action='API'
-                    )
+        print("BEFORE MAP TIHASFDJSBDFKJSDFKJHSDF")
+        route_map.redirect("/", "/dataset")
+        with routes.mapper.SubMapper(
+            route_map, controller="ckanext.csa.plugin:CSAController"
+        ) as m:
+            m.connect("API", "/API", action="API")
         # Attempt to remove /user functionality and rename to different subdomain
         # m.redirect('/hgljkdhfsqhjfhgaslkhjfkjusadh', '/user')
         # m.redirect('/user', '/dataset')
@@ -328,6 +322,7 @@ class CsaPlugin(p.SingletonPlugin, DefaultTranslation):
 
     def get_blueprint(self):
         return blueprint.get_blueprints()
+
     # def get_blueprint(self):
     #     # Create Blueprint for plugin
     #     print('blueprint thing')
@@ -345,6 +340,7 @@ class CsaPlugin(p.SingletonPlugin, DefaultTranslation):
     #         blueprint.add_url_rule(*rule)
 
     #     return blueprint
+
 
 # class CSAController(base.BaseController):
 
--- ckanext/csa/tests/test_plugin.py
+++ ckanext/csa/tests/test_plugin.py
@@ -1,5 +1,6 @@
 """Tests for plugin.py."""
-import ckanext.csa.plugin as plugin
+
+
 
 def test_plugin():
     pass
